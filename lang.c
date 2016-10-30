733a5c1f47bc05841f60b8895d9447814226e3e4
Arda Ünlü <aarda.uunlu@gmail.com>
2016-10-30 22:34:54 +0300

    first

---
+++ lang.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int getinput() {
+    if (feof(stdin)) return EOF;
+    int c = getchar();
+    return c == EOF ? : isalpha(c) ? c : getinput();
+}
+
+int main() {
+    int a, b;
+    a = getinput();
+    b = getinput();
+    while(a != EOF && b != EOF) {
+        putchar(
+            (islower(a) && islower(b))
+              ? (a - b) % 2
+                  ? '-' : '+'
+          : (islower(a) && isupper(b))
+              ? (a - tolower(b)) % 2
+                  ? '.' : ','
+          : (isupper(a) && islower(b))
+              ? (tolower(a) - b) % 2
+                  ? '<' : '>'
+          : (isupper(a) && isupper(b))
+              ? (a - b) % 2
+                  ? '[' : ']'
+          : 0);
+        b = a;
+        a = getinput();
+    }
+}
